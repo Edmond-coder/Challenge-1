fb694bc56520018601edd862175c30bbdf5bf2b6
Edmond-coder <58797298+Edmond-coder@users.noreply.github.com>
2021-07-03 12:47:51 +0200

    new code

--- api.js
+++ api.js
@@ -1,42 +1,61 @@
-const getUserPosts = (id) =>{
- const ul = newTag('ul')
- const closeBtn = newTag('button')
- closeBtn.innerText = 'X'
- closeBtn.addEventListener('click',()=>document.querySelector('ul').style.display='none')
-ul.appendChild(closeBtn)
-
- fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`).
- then(posts => posts.json()).
- then(posts => posts.forEach(({title})=>{
-
-   const li = newTag('li')
-   getTag('posts', ul)
-   li.innerHTML= title
-   ul.appendChild(li)
- }))
-}
-function fetchData() {
-    fetch("https://jsonplaceholder.typicode.com/users").then(response => {
-        if (!response.ok) {
-            throw Error("ERROR");
-        }
-        return response.json();
-    }).then(data => {
-        console.log(data);
-        const html = data.map(user => {
-            return `<div class="user">
-            <p>Name: ${user.name}</p>
-            <p>Email: ${user.email}</p>
-            <button id="${user.id}" onclick="getUserPosts">Get user's posts</button>
-             </div>` 
+document.addEventListener("DOMContentLoaded", function(event) {
+ 
+    const newTag=(tagName)=> document.createElement(tagName);
+    
+    const getTag=(parentElementClass, childTag) => document.getElementsByClassName(parentElementClass)[0].appendChild(childTag)
+    
+   const getUserPosts = (id) =>{
+    const ul = newTag('ul')
+    const closeBtn = newTag('button')
+    closeBtn.innerText = 'X'
+    closeBtn.addEventListener('click',()=>document.querySelector('ul').style.display='none')
+   ul.appendChild(closeBtn)
+   
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`).
+    then(posts => posts.json()).
+    then(posts => posts.forEach(({title})=>{
+   
+      const li = newTag('li')
+      getTag('posts', ul)
+      li.innerHTML= title
+      ul.appendChild(li)
+    }))
+   }
+   
+   
+   function getData() {
+   fetch('https://jsonplaceholder.typicode.com/users',).
+   then(data => data.json()).
+   then(data => {
+      console.log(data)
+       data.forEach(({name, email, id})=>{
+      var div = newTag('div')
+        getTag('user', div);
+   
+        let listName = newTag('h2')
+        let listEmail = newTag('p')
+        let userPostBtn = newTag('button')
+        userPostBtn.innerHTML = 'Get User’s Posts'   
+        userPostBtn.addEventListener('click', function(e) {
+            const list = document.querySelector('ul')
+            if(list){
+                list.parentNode.removeChild(list)
+            }
+            getUserPosts(id)
         })
-        document
-        .querySelector("#api")
-        .insertAdjacentHTML("afterbegin",html);
-    }).catch(error => {
-        console.log(error);
-    });
-}
-
-fetchData();
-
+       
+        div.appendChild(listName)
+        div.appendChild(listEmail)
+        div.appendChild(userPostBtn)
+        listEmail.innerHTML = email
+        listName.innerHTML=name
+       
+    })}).catch(error => {
+    console.log(error);
+   })
+   }
+   
+   getData()
+   
+   })
+  
